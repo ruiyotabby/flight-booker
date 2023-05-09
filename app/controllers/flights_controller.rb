7edbc481a2eea2524ab6897c8b9862155f378607
Ruiyot Abby <ruiyotabby@gmail.com>
2023-05-10 01:41:38 +0300

    Add #index action to the flight controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,8 @@
 class FlightsController < ApplicationController
+  def index
+    @flights = Flight.includes(:departure_airport, :arrival_airport).all
+    @departure_options = @flights.map { |f| [f.departure_airport.code, f.departure_airport.id] }
+    @arrival_options = @flights.map { |f| [f.arrival_airport.code, f.arrival_airport.id] }
+    @start_options = @flights.map { |f| [f.start.to_fs(:long), f.id] }
+  end
 end
