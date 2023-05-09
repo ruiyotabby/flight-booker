671ec2fa82a603ec49851a6dd22fb16866f4c648
Ruiyot Abby <ruiyotabby@gmail.com>
2023-05-09 19:08:59 +0300

    Add associations wih the airport model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,6 @@
 class Flight < ApplicationRecord
+  belongs_to :departure_airport, class_name: "Airport", foreign_key: "departure_airport_id"
+  belongs_to :arrival_airport, class_name: "Airport", foreign_key: "arrival_airport_id"
+  # validates :start, presence: true
+  # validates :duration, presence: true
 end
