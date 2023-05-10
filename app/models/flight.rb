15ad935eb774b6bd0d0d555427530bdaa8d3c4ca
Ruiyot Abby <ruiyotabby@gmail.com>
2023-05-10 21:34:09 +0300

    Add a scope which performs flight search from user parameters

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,7 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport", foreign_key: "arrival_airport_id"
   # validates :start, presence: true
   # validates :duration, presence: true
+
+  scope :matching_airports_and_start, ->(departure_airport_id, arrival_airport_id, start) {
+    where(departure_airport_id: departure_airport_id, arrival_airport_id: arrival_airport_id, start: start)}
 end
