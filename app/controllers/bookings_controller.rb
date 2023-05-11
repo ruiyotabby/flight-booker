7bd53984fa7cb41a807e8f690e5083d54e18e542
Ruiyot Abby <ruiyotabby@gmail.com>
2023-05-11 21:09:49 +0300

    Add new and create action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
+    params[:tickets].to_i.times{ @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    puts booking_params
+
+    if @booking.save
+      redirect_to flights_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :name, :email])
+  end
 end
